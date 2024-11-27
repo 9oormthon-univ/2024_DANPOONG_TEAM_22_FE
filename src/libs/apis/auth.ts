c186ab3287827b83c4e5cfd7188407745789bd05
salmonco <xkqjsslsek80@naver.com>
2024-11-27 16:52:59 +0900

    chore/react-natvie-config 설정이 안 돼서 임시 api url 기입

--- src/libs/apis/auth.ts
+++ src/libs/apis/auth.ts
@@ -1,15 +1,15 @@
-import { LoginRequestData, LoginResponseData } from '@type/auth'
-import { ResultResponseData } from '@type/common'
-import axios from 'axios'
+import {LoginRequestData, LoginResponseData} from '@type/auth';
+import {ResultResponseData} from '@type/common';
+import axios from 'axios';
 
 const postLogin = async ({
   accessToken,
   loginType,
 }: Readonly<LoginRequestData>) => {
   const res = await axios.post<ResultResponseData<LoginResponseData>>(
-    `${process.env.EXPO_PUBLIC_API_URL}/api/v1/auth/login?accessToken=${accessToken}&loginType=${loginType}`
-  )
-  return res.data
-}
+    `${'http://nysams.com:8081'}/api/v1/auth/login?accessToken=${accessToken}&loginType=${loginType}`,
+  );
+  return res.data;
+};
 
-export { postLogin }
+export {postLogin};
