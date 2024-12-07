e2c58d88ca7cd5448bacabc67015eb9930b58ea2
salmonco <xkqjsslsek80@naver.com>
2024-12-07 17:54:27 +0900

    chore/편지 목데이터

--- src/screens/Letter/LetterList/index.tsx
+++ src/screens/Letter/LetterList/index.tsx
@@ -3,7 +3,7 @@ import {getLetters} from '@apis/providedFile';
 // import {getTopText} from '@apis/RCDApis/getTopText';
 import AppBar from '@components/atom/AppBar';
 import BG from '@components/atom/BG';
-// import {LETTERS_DATA} from '@constants/letter';
+import {LETTERS_DATA} from '@constants/letter';
 // import useGetAlarmCategory from '@hooks/alarm/useGetAlarmCategory';
 // import useGetAlarmComfort from '@hooks/alarm/useGetAlarmComfort';
 import Txt from '@components/atom/Txt';
@@ -74,10 +74,10 @@ const LetterListScreen = ({navigation}: Readonly<LetterProps>) => {
           pageable: {page: 0, size: 10, sort: 'createdAt,desc'},
         });
         console.log(res);
-        setLettersData(res.result.content);
-        setFilteredLettersData(res.result.content);
-        // setLettersData(LETTERS_DATA);
-        // setFilteredLettersData(LETTERS_DATA);
+        // setLettersData(res.result.content);
+        // setFilteredLettersData(res.result.content);
+        setLettersData(LETTERS_DATA);
+        setFilteredLettersData(LETTERS_DATA);
       } catch (error) {
         console.error(error);
         Alert.alert('오류', '편지 정보를 불러오는 중 오류가 발생했어요');
@@ -102,64 +102,64 @@ const LetterListScreen = ({navigation}: Readonly<LetterProps>) => {
   }, [selectedFilterIdx]);
 
   return (
-      <BG type="main">
-        <View className="flex-1">
-          {/* AppBar는 flex-1 컨테이너 안에 넣어야 함 */}
-          <AppBar
-            title="청년의 편지"
-            goBackCallbackFn={() => navigation.goBack()}
-            className="absolute top-[6] w-full"
-          />
-          <StatusBarGap />
-          <View>
-            <ScrollView
-              horizontal
-              showsHorizontalScrollIndicator={false}
-              className="mt-[74]">
-              <View className="flex-row items-center px-[30] py-[10] h-[36] mt-[20]">
-                {parentCategories.map((menu, index) => (
-                  <Pressable
-                    key={`${menu.category}=${menu.label}-parentCategories`}
-                    className={`h-[36] px-[22] items-center justify-center border ${
+    <BG type="main">
+      <View className="flex-1">
+        {/* AppBar는 flex-1 컨테이너 안에 넣어야 함 */}
+        <AppBar
+          title="청년의 편지"
+          goBackCallbackFn={() => navigation.goBack()}
+          className="absolute top-[6] w-full"
+        />
+        <StatusBarGap />
+        <View>
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            className="mt-[74]">
+            <View className="flex-row items-center px-[30] py-[10] h-[36] mt-[20]">
+              {parentCategories.map((menu, index) => (
+                <Pressable
+                  key={`${menu.category}=${menu.label}-parentCategories`}
+                  className={`h-[36] px-[22] items-center justify-center border ${
+                    index === selectedFilterIdx
+                      ? 'border-tabIcon bg-white/10'
+                      : 'border-white10'
+                  } mr-[8]`}
+                  style={{borderRadius: 20}}
+                  onPress={() => setSelectedFilterIdx(index)}>
+                  <Txt
+                    type="body4"
+                    text={menu.label}
+                    className={`${
                       index === selectedFilterIdx
-                        ? 'border-tabIcon bg-white/10'
-                        : 'border-white10'
-                    } mr-[8]`}
-                    style={{borderRadius: 20}}
-                    onPress={() => setSelectedFilterIdx(index)}>
-                    <Txt
-                      type="body4"
-                      text={menu.label}
-                      className={`${
-                        index === selectedFilterIdx
-                          ? 'text-yellowPrimary'
-                          : 'text-gray300'
-                      }`}
-                    />
-                  </Pressable>
-                ))}
-              </View>
-            </ScrollView>
-          </View>
+                        ? 'text-yellowPrimary'
+                        : 'text-gray300'
+                    }`}
+                  />
+                </Pressable>
+              ))}
+            </View>
+          </ScrollView>
+        </View>
 
-          <View className="flex-row items-center mt-[30] px-[30]">
-            <Text
-              className="text-white"
-              style={{
-                fontSize: 22,
-                fontFamily: 'LeeSeoyun-Regular',
-                lineHeight: 22 * 1.5,
-              }}>
-              TO.
-            </Text>
-            <Txt
-              type="title4"
-              text={nickname ?? ''}
-              className="text-yellowPrimary ml-[7]"
-            />
-          </View>
-          {/* 목 데이터 넣은 버전 */}
-          {/* <ScrollView>
+        <View className="flex-row items-center mt-[30] px-[30]">
+          <Text
+            className="text-white"
+            style={{
+              fontSize: 22,
+              fontFamily: 'LeeSeoyun-Regular',
+              lineHeight: 22 * 1.5,
+            }}>
+            TO.
+          </Text>
+          <Txt
+            type="title4"
+            text={nickname ?? ''}
+            className="text-yellowPrimary ml-[7]"
+          />
+        </View>
+        {/* 목 데이터 넣은 버전 */}
+        {/* <ScrollView>
             <View className="pt-[22] px-[30] pb-[110]">
               {(!filteredLettersData || filteredLettersData.length === 0 ? LETTERS_DATA : filteredLettersData).map(
                 (letter, idx) => (
@@ -171,29 +171,29 @@ const LetterListScreen = ({navigation}: Readonly<LetterProps>) => {
               )}
             </View>
           </ScrollView> */}
-          {!filteredLettersData || filteredLettersData.length === 0 ? (
-            <View className="flex-1 items-center justify-center">
-              <Txt
-                type="caption1"
-                text="아직 편지가 없어요"
-                className="text-gray300"
-              />
+        {!filteredLettersData || filteredLettersData.length === 0 ? (
+          <View className="flex-1 items-center justify-center">
+            <Txt
+              type="caption1"
+              text="아직 편지가 없어요"
+              className="text-gray300"
+            />
+          </View>
+        ) : (
+          <ScrollView>
+            <View className="pt-[22] px-[30] pb-[110]">
+              {filteredLettersData.map((letter, idx) => (
+                <View
+                  key={`${letter.createdAt}-${letter.providedFileId}-filteredLettersData`}>
+                  <LetterCard letter={letter} idx={idx} />
+                  <View className="mb-[30]" />
+                </View>
+              ))}
             </View>
-          ) : (
-            <ScrollView>
-              <View className="pt-[22] px-[30] pb-[110]">
-                {filteredLettersData.map((letter, idx) => (
-                  <View
-                    key={`${letter.createdAt}-${letter.providedFileId}-filteredLettersData`}>
-                    <LetterCard letter={letter} idx={idx} />
-                    <View className="mb-[30]" />
-                  </View>
-                ))}
-              </View>
-            </ScrollView>
-          )}
-        </View>
-      </BG>
+          </ScrollView>
+        )}
+      </View>
+    </BG>
   );
 };
 
