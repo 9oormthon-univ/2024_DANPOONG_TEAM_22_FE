f995aca9cef3af31d7eddd4dba08eb25ec06bb3e
salmonco <xkqjsslsek80@naver.com>
2025-02-12 03:12:52 +0900

    design/역할 선택

--- src/screens/Login/RoleSelect/index.tsx
+++ src/screens/Login/RoleSelect/index.tsx
@@ -21,43 +21,36 @@ const RoleSelectScreen = ({navigation}: Readonly<AuthProps>) => {
 
   return (
     <BG type="main">
-      <View className="items-center pt-[110]">
-        <Txt
-          type="body3"
-          text="이곳은 광활한 사막..."
-          className="text-gray300"
-        />
-        <Txt type="title2" text={`${''} 님,`} className="text-white mt-[26]" />
+      <View className="items-center pt-[170]">
         <Txt type="title2" text="당신은 누구인가요?" className="text-white" />
-
-        <View className="mt-[30] px-[46] flex-row">
+        <View className="mt-[50] px-[46] flex-row">
           <Pressable
             className={`relative w-1/2 pt-[41] h-[180] items-center mr-[22] border ${
               role === 'HELPER'
-                ? 'bg-white/20 border-yellowPrimary'
-                : 'bg-white/10 border-white/10'
+                ? 'border-yellowPrimary bg-yellow300/15'
+                : 'border-gray300 bg-white/10'
             }`}
             style={{borderRadius: 10}}
             onPress={() => setRole('HELPER')}>
             <Txt
-              type="title3"
+              type="title4"
               text="조력자"
               className="text-white mb-[30] text-center"
             />
-            <View className="absolute bottom-0">
+            <View className="absolute bottom-[14]">
               <VolunteerIcon />
             </View>
           </Pressable>
           <Pressable
             className={`relative w-1/2 pt-[41] h-[180] items-center border ${
               role === 'YOUTH'
-                ? 'bg-white/20 border-yellowPrimary'
-                : 'bg-white/10 border-white/10'
+                ? 'border-yellowPrimary bg-yellow300/15'
+                : 'border-gray300 bg-white/10'
             }`}
             style={{borderRadius: 10}}
             onPress={() => setRole('YOUTH')}>
             <Txt
-              type="title3"
+              type="title4"
               text="청년"
               className="text-white mb-[30] text-center"
             />
@@ -67,9 +60,49 @@ const RoleSelectScreen = ({navigation}: Readonly<AuthProps>) => {
           </Pressable>
         </View>
       </View>
+      <View className="justify-center mt-[50]">
+        <View className="flex-row justify-center">
+          <View className="flex-row items-center justify-center">
+            <Txt
+              type="caption1"
+              text={`내일모래에는 `}
+              className="text-gray300 text-center"
+            />
+            <Txt
+              type="caption1"
+              text={`선한 마음을 담아 목소리를 녹음하는 분`}
+              className={`${
+                role === 'HELPER' ? 'text-yellowPrimary' : 'text-gray300'
+              } text-center`}
+            />
+            <Txt
+              type="caption1"
+              text={`들과,`}
+              className="text-gray300 text-center"
+            />
+          </View>
+        </View>
+        <View className="flex-row justify-center">
+          <View className="flex-row justify-center">
+            <Txt
+              type="caption1"
+              text={`이 목소리를 들으며 용기를 얻는 분들`}
+              className={`${
+                role === 'YOUTH' ? 'text-yellowPrimary' : 'text-gray300'
+              } text-center`}
+            />
+            <Txt
+              type="caption1"
+              text={`이 있어요`}
+              className="text-gray300 text-center"
+            />
+          </View>
+        </View>
+      </View>
+
       <Image
         source={require('@assets/pngs/background/signup2.png')}
-        className="w-full h-auto flex-1 mt-[54]"
+        className="w-full h-auto mt-[54]"
       />
       <View className="absolute left-0 bottom-[30] w-full px-[40]">
         <Button text="다음" onPress={handleNext} disabled={!role} />
