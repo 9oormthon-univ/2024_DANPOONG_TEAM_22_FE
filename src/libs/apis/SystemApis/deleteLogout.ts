8e9ed7b3efba9a5a02c462b1b8259284180aeb32
salmonco <xkqjsslsek80@naver.com>
2025-02-17 22:43:45 +0900

    fix/api 요청 무한루프 방지

--- src/libs/apis/SystemApis/deleteLogout.ts
+++ src/libs/apis/SystemApis/deleteLogout.ts
@@ -1,18 +1,26 @@
-import client from '@apis/client';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
 
 interface DeleteLogoutResponse {
-    timestamp: string;
-    code: string;
-    message: string;
-    result: {
-        memberId: number;
-    };
+  timestamp: string;
+  code: string;
+  message: string;
+  result: {
+    memberId: number;
+  };
 }
 
 export const deleteLogout = async (): Promise<DeleteLogoutResponse> => {
   try {
-    const response = await client.delete<DeleteLogoutResponse>(
+    const accessToken = await AsyncStorage.getItem('accessToken');
+
+    const response = await axios.delete<DeleteLogoutResponse>(
       '/api/v1/auth/logout',
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
     );
     return response.data;
   } catch (error) {
--- src/libs/hooks/auth/useGetMember.ts
+++ src/libs/hooks/auth/useGetMember.ts
@@ -1,8 +1,12 @@
 import {getMember} from '@apis/member';
 import {useQuery} from '@tanstack/react-query';
 
-const useGetMember = () => {
-  return useQuery({queryKey: ['getMember'], queryFn: () => getMember()});
+const useGetMember = (token: string | null) => {
+  return useQuery({
+    queryKey: ['getMember'],
+    queryFn: () => getMember(),
+    enabled: token !== null,
+  });
 };
 
 export default useGetMember;
