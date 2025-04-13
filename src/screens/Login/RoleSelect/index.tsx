38096abaac103f30a2b5c77cc6c8db312912f88d
salmonco <xkqjsslsek80@naver.com>
2025-04-13 22:07:39 +0900

    chore/lint fix

--- src/screens/Login/RoleSelect/index.tsx
+++ src/screens/Login/RoleSelect/index.tsx
@@ -1,30 +1,37 @@
+import { useState } from 'react';
+import { Image, Pressable, View } from 'react-native';
+
+import { BG } from '@components/BG';
+import { Button } from '@components/Button';
+import { CustomText } from '@components/CustomText';
+import { type NativeStackScreenProps } from '@react-navigation/native-stack';
+import { type AuthStackParamList } from '@stackNav/Auth';
+import { type Role } from '@type/api/common';
+import { trackEvent } from '@utils/tracker';
+
 import VolunteerIcon from '@assets/svgs/volunteer.svg';
 import YouthIcon from '@assets/svgs/youth.svg';
-import {BG} from '@components/BG';
-import {Button} from '@components/Button';
-import {CustomText} from '@components/CustomText';
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {AuthStackParamList} from '@stackNav/Auth';
-import {Role} from '@type/api/common';
-import {trackEvent} from '@utils/tracker';
-import {useState} from 'react';
-import {Image, Pressable, View} from 'react-native';
 
 type AuthProps = NativeStackScreenProps<AuthStackParamList, 'RoleSelectScreen'>;
 
-export const RoleSelectScreen = ({navigation}: Readonly<AuthProps>) => {
+export const RoleSelectScreen = ({ navigation }: Readonly<AuthProps>) => {
   const [role, setRole] = useState<Role | null>(null);
 
   const handleNext = () => {
     if (!role) return;
+
     trackEvent('usertype_selected');
-    navigation.navigate('NicknameWriteScreen', {role});
+    navigation.navigate('NicknameWriteScreen', { role });
   };
 
   return (
     <BG type="main">
       <View className="items-center pt-[170]">
-        <CustomText type="title2" text="당신은 누구인가요?" className="text-white" />
+        <CustomText
+          type="title2"
+          text="당신은 누구인가요?"
+          className="text-white"
+        />
         <View className="mt-[50] px-[46] flex-row">
           <Pressable
             className={`relative w-1/2 pt-[41] h-[180] items-center mr-[22] border ${
@@ -32,9 +39,10 @@ export const RoleSelectScreen = ({navigation}: Readonly<AuthProps>) => {
                 ? 'border-yellowPrimary bg-yellow300/15'
                 : 'border-gray300 bg-white/10'
             }`}
-            style={{borderRadius: 10}}
+            style={{ borderRadius: 10 }}
             onPress={() => setRole('HELPER')}>
-            <CustomText              type="title4"
+            <CustomText
+              type="title4"
               text="조력자"
               className="text-white mb-[30] text-center"
             />
@@ -48,9 +56,10 @@ export const RoleSelectScreen = ({navigation}: Readonly<AuthProps>) => {
                 ? 'border-yellowPrimary bg-yellow300/15'
                 : 'border-gray300 bg-white/10'
             }`}
-            style={{borderRadius: 10}}
+            style={{ borderRadius: 10 }}
             onPress={() => setRole('YOUTH')}>
-            <CustomText              type="title4"
+            <CustomText
+              type="title4"
               text="청년"
               className="text-white mb-[30] text-center"
             />
@@ -63,17 +72,20 @@ export const RoleSelectScreen = ({navigation}: Readonly<AuthProps>) => {
       <View className="justify-center mt-[50]">
         <View className="flex-row justify-center">
           <View className="flex-row items-center justify-center">
-            <CustomText              type="caption1"
+            <CustomText
+              type="caption1"
               text={`내일모래에는 `}
               className="text-gray300 text-center"
             />
-            <CustomText              type="caption1"
+            <CustomText
+              type="caption1"
               text={`선한 마음을 담아 목소리를 녹음하는 분`}
               className={`${
                 role === 'HELPER' ? 'text-yellowPrimary' : 'text-gray300'
               } text-center`}
             />
-            <CustomText              type="caption1"
+            <CustomText
+              type="caption1"
               text={`들과,`}
               className="text-gray300 text-center"
             />
@@ -81,13 +93,15 @@ export const RoleSelectScreen = ({navigation}: Readonly<AuthProps>) => {
         </View>
         <View className="flex-row justify-center">
           <View className="flex-row justify-center">
-            <CustomText              type="caption1"
+            <CustomText
+              type="caption1"
               text={`이 목소리를 들으며 용기를 얻는 분들`}
               className={`${
                 role === 'YOUTH' ? 'text-yellowPrimary' : 'text-gray300'
               } text-center`}
             />
-            <CustomText              type="caption1"
+            <CustomText
+              type="caption1"
               text={`이 있어요`}
               className="text-gray300 text-center"
             />
